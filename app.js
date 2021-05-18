cd199ac6a8c25d9077455bfc3cb5edf0f8122a57
Ian <iansutherland.az@gmail.com>
2021-05-18 07:35:54 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+axios =  require('axios');
+const jsdom = require("jsdom");
+const {JSDOM} = jsdom;
+
+async function formatTableData() {
+    const page = await callApi()
+    const dom = new JSDOM(page.data);
+
+    //get table element
+    const tableElem = dom.window.document.querySelector('.table');
+    const tableContent = tableElem ?tableElem.textContent : null;
+
+    //get table headers
+    const firstRow = tableElem.querySelector('tr');
+    const headers = firstRow.textContent.trim().split('\n\t');
+
+    //get rid of 'shar event' column used for social media links
+    headers.pop(-1);
+
+    //get the other rows
+    const dataRows = Array.from(tableElem.querySelectorAll('tr'))
+        .map(x => x.textContent.replace('\t', '').split('\n').filter(x => x !== '').splice(0, 5));
+    const dataSansHeaders = dataRows.splice(1, dataRows.length);
+    
+    return {
+        headers: headers,
+        data: dataSansHeaders
+    }
+}
+
+async function callApi() {
+    const result = await axios.get(`https://spotthestation.nasa.gov/sightings/view.cfm?country=United_States&region=Arizona&city=Phoenix`);
+    if(result.statusCode === 200){
+        return result.data;
+    }
+    else{
+        return result
+    }
+}
+
+formatTableData().then(x => console.log(x));
